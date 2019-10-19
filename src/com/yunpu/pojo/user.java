21b9cc78be271184c75474c3365dc57fe0fba214
ssrsfyb <1483824245@qq.com>
2019-10-19 11:27:59 +0800

    往github上传了密钥，再次试验

--- src/com/yunpu/pojo/user.java
+++ src/com/yunpu/pojo/user.java
@@ -2,7 +2,7 @@ package com.yunpu.pojo;
 
 public class user {
     public static void main(String[] args) {
-        System.out.println("1111");
+        System.out.println("往github上传了密钥");
         System.out.println("1次修改，上次没有成功..");
         System.out.println("Hello word");
     }
