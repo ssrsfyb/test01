620c1692bd6972eb26e75a9a62eddf1f6f78d18d
ssrsfyb <1483824245@qq.com>
2019-10-19 11:50:23 +0800

    往github上传了密钥，再次试验

--- src/com/yunpu/pojo/user.java
+++ src/com/yunpu/pojo/user.java
@@ -2,6 +2,7 @@ package com.yunpu.pojo;
 
 public class user {
     public static void main(String[] args) {
+        System.out.println("11111");
         System.out.println("hello nihao");
         System.out.println("Hello word");
     }
