e6ac3142516733f6aa56c128d1076de9e033d24c
ssrsfyb <1483824245@qq.com>
2019-10-19 14:54:21 +0800

    又不好使了，接着来

--- src/com/yunpu/pojo/user.java
+++ src/com/yunpu/pojo/user.java
@@ -2,6 +2,7 @@ package com.yunpu.pojo;
 
 public class user {
     public static void main(String[] args) {
+        System.out.println("第二次没好使，接着来");
         System.out.println("第二次实验提交,puth");
         System.out.println("hello nihao");
         System.out.println("Hello word");
