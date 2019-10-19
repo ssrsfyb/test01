4cfeb60eac88ac347b4591d3bc1e0a5e4bfbba21
ssrsfyb <1483824245@qq.com>
2019-10-19 14:51:08 +0800

    不知道怎么就可以用了

--- src/com/yunpu/pojo/user.java
+++ src/com/yunpu/pojo/user.java
@@ -2,7 +2,7 @@ package com.yunpu.pojo;
 
 public class user {
     public static void main(String[] args) {
-        System.out.println("第二次实验提交");
+        System.out.println("第二次实验提交,puth");
         System.out.println("hello nihao");
         System.out.println("Hello word");
     }
